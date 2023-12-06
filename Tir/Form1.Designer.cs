c07e2e27d19fc2f390213bda4e93318d721efc8a
Kseniia Dmitrievna <kseniiaastakhova@gmail.com>
2023-12-06 21:48:12 +0300

    working version

---
+++ Tir/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Tir
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tBoxHeight = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBoxTime = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tBoxWidth = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(22, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 562);
+            this.panel1.TabIndex = 26;
+            // 
+            // tBoxHeight
+            // 
+            this.tBoxHeight.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.tBoxHeight.Location = new System.Drawing.Point(122, 56);
+            this.tBoxHeight.Multiline = true;
+            this.tBoxHeight.Name = "tBoxHeight";
+            this.tBoxHeight.Size = new System.Drawing.Size(86, 31);
+            this.tBoxHeight.TabIndex = 21;
+            this.tBoxHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnStop.Location = new System.Drawing.Point(504, 56);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(80, 31);
+            this.btnStop.TabIndex = 18;
+            this.btnStop.Text = "Сначала";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnStart.Location = new System.Drawing.Point(418, 56);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 31);
+            this.btnStart.TabIndex = 25;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 32);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Выберите скорость\r\nперемещения мишени:\r\n";
+            // 
+            // cmbBoxTime
+            // 
+            this.cmbBoxTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBoxTime.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.cmbBoxTime.FormattingEnabled = true;
+            this.cmbBoxTime.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cmbBoxTime.Location = new System.Drawing.Point(292, 63);
+            this.cmbBoxTime.Name = "cmbBoxTime";
+            this.cmbBoxTime.Size = new System.Drawing.Size(94, 24);
+            this.cmbBoxTime.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Введите размер:";
+            // 
+            // tBoxWidth
+            // 
+            this.tBoxWidth.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.tBoxWidth.Location = new System.Drawing.Point(22, 56);
+            this.tBoxWidth.Multiline = true;
+            this.tBoxWidth.Name = "tBoxWidth";
+            this.tBoxWidth.Size = new System.Drawing.Size(86, 31);
+            this.tBoxWidth.TabIndex = 20;
+            this.tBoxWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.progressBar.Location = new System.Drawing.Point(28, 705);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(556, 33);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 19;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 765);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tBoxHeight);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBoxTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBoxWidth);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Тир";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tBoxHeight;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBoxTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBoxWidth;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Tir/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tir
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] ArrayButton;
+        List<Button> listButton = new List<Button>();
+        Random rnd = new Random();
+        Button btnNow = new Button();
+        Button btnAfter = new Button();
+        int targetRedLast, noun, targetRed, targetAmount, targetKill, width, height, speed;
+
+        
+
+        bool flag;
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                width = Convert.ToInt32(tBoxWidth.Text);
+                height = Convert.ToInt32(tBoxHeight.Text);
+                if (width > 15 | height > 15)
+                {
+                    MessageBox.Show("Размер не должен превышать 15!");
+                    Application.Restart();
+                }
+                generationMishen(width, height);
+                targetAmount = width * height;
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели неверный формат размера. Попробуйте снова.");
+                Application.Restart();
+            }
+        }
+
+        public void generationMishen(int width, int height)
+        {
+            ArrayButton = new Button[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    ArrayButton[i, j] = new Button();
+                    ArrayButton[i, j].Location = new Point(i * 30, j * 30);
+                    ArrayButton[i, j].Size = new Size(25, 25);
+                    ArrayButton[i, j].BackColor = Color.Blue;
+
+                    panel1.Controls.Add(ArrayButton[i, j]);
+
+                    ArrayButton[i, j].Click += Click_Picture;
+
+                    listButton.Add(ArrayButton[i, j]);
+                }
+            }
+
+           
+            if (cmbBoxTime.Text == "1") { speed = 2000; }
+            else if (cmbBoxTime.Text == "2") { speed = 1000; }
+            else { speed = 800; }
+
+            timer.Interval = speed;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(Timer_Tick);
+
+            progressBar.Visible = true;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = ArrayButton.Length;
+            progressBar.Value = 0;
+            progressBar.Step = 1;
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void Click_Picture(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((sender as Button).BackColor == Color.Red)
+                {
+                    noun++;
+                    progressBar.PerformStep();
+                }
+            }
+            catch { }
+
+            listButton.Remove(sender as Button);
+
+            (sender as Button).Visible = false;
+            btnNow = null;
+
+            targetKill++;
+
+            if (targetKill == targetAmount)
+            {
+                timer.Stop();
+                MessageBox.Show(Convert.ToString("Набрано очков:  " + Convert.ToString(noun)));
+                Application.Restart();
+            }
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (listButton.Count == 1 & flag == false)
+            {
+                listButton[0].BackColor = Color.Red;
+
+            }
+            else if (listButton.Count == 1 & flag == true)
+            {
+                listButton[0].BackColor = Color.Blue;
+            }
+            else
+            {
+                while (targetRed == targetRedLast)
+                {
+                    targetRed = rnd.Next(0, listButton.Count);
+                }
+
+                listButton[targetRed].BackColor = Color.Red;
+                btnNow = listButton[targetRed];
+                if (btnAfter != null)
+                {
+                    btnAfter.BackColor = Color.Blue;
+                }
+
+                targetRedLast = targetRed;
+                btnAfter = btnNow;
+            }
+            if (flag == true) { flag = false; }
+            else { flag = true; }
+        }
+    }
+}
